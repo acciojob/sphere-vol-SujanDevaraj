dc99c744622cfb125d3a0d803637b1a74235e80a
SujanDevaraj <96370355+SujanDevaraj@users.noreply.github.com>
2023-09-18 20:57:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function volume_sphere() {
     //Write your code here
+	var radius = document.getElementById("radius").value;
 	let f=4/3;
-	let v=f*Math.PI**r;
-  
+	let v=f*Math.PI*Math.pow(radius, 3);
+    document.getElementById("volume").innerHTML =  v.toFixed(2);
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
