24b2e9d4abd5e54523cb3ae377494d2731b916f8
SujanDevaraj <96370355+SujanDevaraj@users.noreply.github.com>
2023-09-18 21:02:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 function volume_sphere() {
     //Write your code here
-	var radius = document.getElementById("radius").value;
-	let f=4/3;
-	let v=f*Math.PI*Math.pow(radius, 3);
-    document.getElementById("volume").innerHTML =  v.toFixed(2);
+	 var radius = parseFloat(document.getElementById("radius").value);
+
+            // Check if the input is a valid number
+            if (isNaN(radius)) {
+                document.getElementById("volume").textContent = "Please enter a valid number for the radius.";
+            } else {
+                // Calculate the volume of the sphere
+                var volume = (4/3) * Math.PI * Math.pow(radius, 3);
+
+                // Display the result in the "volume" paragraph
+                document.getElementById("volume").textContent = "The volume of the sphere is " + volume.toFixed(2);
+            }
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
